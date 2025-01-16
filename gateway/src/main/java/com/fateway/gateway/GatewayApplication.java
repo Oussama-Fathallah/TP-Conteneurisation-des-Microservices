de34841c744a42a892010f0ac923e683e2a63474
Oussama Fathallah <oussama.fathallah05@gmail.com>
2025-01-16 14:54:04 +0100

    test jenkins

--- gateway/src/main/java/com/fateway/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/fateway/gateway/GatewayApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+  //test jenkins
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
